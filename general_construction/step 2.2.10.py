8c1aa31c94f3d1979977228e0f39b424bc7d20a0
Левон Абрамян <abrleva8@gmail.com>
2021-07-10 23:54:54 +0300

    anton

--- general_construction/step 2.2.10.py
+++ general_construction/step 2.2.10.py
@@ -7,17 +7,20 @@
 # то холодильник заражен и нужно вывести номер холодильника, нумерация начинается с единицы
 
 def some_func(string):
-    if string.count('n') != 2:
+    if string.count('n') < 2:
         return False
     arr = list('anton')
     index = 0
     for ch in arr:
         if string.find(ch, index) > -1:
-            index = string.find(index, 'ch')
+            index = string.find(ch, index)
+        else:
+            return False
+    return True
 
 
 n = int(input())
-print(i + 1 if some_func() else '' for i in range(n))
+# print(i + 1 if some_func() else '' for i in range(n))
 for i in range(n):
     if some_func(input()):
-        print(i + 1)
+        print(i + 1, end=' ')
