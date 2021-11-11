3ac62cb09d6592cb2e3d839cf871a126b96000d5
Левон Абрамян <labramyan404@mso365.technolog.edu.ru>
2021-11-12 00:16:17 +0300

    more than average

---
+++ nested_lists_part_2/step 4.4.4.py
@@ -0,0 +1,28 @@
+# Напишите программу, которая выводит количество элементов квадратной матрицы в каждой строке,
+# больших среднего арифметического элементов данной строки.
+#
+# Формат входных данных
+# На вход программе подаётся натуральное число n — количество строк и столбцов в матрице,
+# затем элементы матрицы (целые числа) построчно через пробел.
+#
+# Формат выходных данных
+# Программа должна вывести n чисел — для каждой строки количество элементов матрицы,
+# больших среднего арифметического элементов данной строки.
+def read_matrix(n):
+    return [[int(el) for el in input().split()] for _ in range(n)]
+
+
+def print_count_more_than_average(li):
+    mean = sum(li) / len(li)
+    return sum([1 if el > mean else 0 for el in li])
+
+
+def print_result(matrix):
+    for el in matrix:
+        print(print_count_more_than_average(el))
+
+
+n = int(input())
+
+matrix = read_matrix(n)
+print_result(matrix)
