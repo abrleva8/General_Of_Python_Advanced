8f327a1eac264ccb0a0c17e7bc9f06a59e6aa496
abrleva8@gmail.com <pT6n6TFrSFjcJAu>
2022-01-12 23:35:42 +0300

    filling_5

---
+++ nested_lists_part_2/step 4.6.7.py
@@ -0,0 +1,17 @@
+# На вход программе подаются два натуральных числа n и m. Напишите программу, которая создает матрицу размером n×m
+# и заполняет её числами от 1 до n⋅m в соответствии с образцом.
+
+def fill_matrix(length: int, width: int):
+    return [[(j + i + 1) % width if (j + i + 1) % width != 0 else width for j in range(width)] for i in range(length)]
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        for el in row:
+            print(str(el).ljust(3), end=' ')
+        print()
+
+
+n, m = map(int, input().split())
+matrix = fill_matrix(n, m)
+print_matrix(matrix)
